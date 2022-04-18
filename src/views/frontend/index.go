47f0b60c029de29d60acfc7e96912524dca3d8b8
megahomyak <g.megahomyak@gmail.com>
2022-04-19 00:50:03 +0500

    Arguments in `index.tmpl` are in lowerCamelCase now

--- src/views/frontend/index.go
+++ src/views/frontend/index.go
@@ -11,12 +11,12 @@ func Index(c *gin.Context) {
 	user := utils.GetUser(c, utils.WithNotes)
 	if user == nil {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"UserIsLoggedIn": false,
+			"userIsLoggedIn": false,
 		})
 	} else {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"UserIsLoggedIn": true,
-			"Notes": user.Notes,
+			"userIsLoggedIn": true,
+			"notes": user.Notes,
 		})
 	}
 }
