7b8e89fbcb61fe240019b727601f630d397348f7
megahomyak <g.megahomyak@gmail.com>
2022-04-19 12:44:49 +0500

    All fields are NOT NULL now

--- src/models/models.go
+++ src/models/models.go
@@ -4,17 +4,17 @@ import "database/sql"
 
 type User struct {
 	ID          uint64
-	FirstName   string
-	LastName    string
-	AccessToken sql.NullString `gorm:"uniqueIndex"`
-	JWTSubject  string         `gorm:"uniqueIndex"`
+	FirstName   string         `gorm:"not null"`
+	LastName    string         `gorm:"not null"`
+	AccessToken sql.NullString `gorm:"uniqueIndex;not null"`
+	JWTSubject  string         `gorm:"uniqueIndex;not null"`
 	Notes       *[]Note
 }
 
 type Note struct {
 	ID       uint64
-	Name     string
-	Contents string
-	UserID   uint64
-	Owner    *User `gorm:"ForeignKey:UserID"`
+	Name     string `gorm:"not null"`
+	Contents string `gorm:"not null"`
+	UserID   uint64 `gorm:"not null"`
+	Owner    *User  `gorm:"ForeignKey:UserID"`
 }
